64c680dc0f5cb72aa5c714e87fe4282807128426
eudikatech <118651920+eudikatech@users.noreply.github.com>
2022-11-30 12:19:22 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,11 +12,6 @@ import Mappage from "../components/Mappage"
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>FloraBangla Flower Park</title>
-        <meta name="description" content="Flower Park with Resort" />
-        <link rel="icon" href="/flowerparkicon.ico" />
-      </Head>
 
       <main className={styles.main}>
         <Hero />
